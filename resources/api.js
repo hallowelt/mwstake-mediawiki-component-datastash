74cb7a48c6f92b0e2e8b14f54ba8af054adff77e
Hamish <hamish.slater@hallowelt.com>
2026-06-24 13:09:39 +0700

    eslint

--- resources/api.js
+++ resources/api.js
@@ -1,20 +1,12 @@
 mws = window.mws || {};
 mws.datastash = {
-	get: async ( key ) => {
-		return mws.datastash.getInternal( key, false );
-	},
-	getGlobal: async ( key ) => {
-		return mws.datastash.getInternal( key, true );
-	},
-	set: async ( key, value ) => {
-		return mws.datastash.setInternal( key, value, false );
-	},
-	setGlobal: async ( key, value ) => {
-		return mws.datastash.setInternal( key, value, true );
-	},
+	get: async ( key ) => mws.datastash.getInternal( key, false ),
+	getGlobal: async ( key ) => mws.datastash.getInternal( key, true ),
+	set: async ( key, value ) => mws.datastash.setInternal( key, value, false ),
+	setGlobal: async ( key, value ) => mws.datastash.setInternal( key, value, true ),
 	getInternal: async ( key, getGlobal ) => {
 		const restScript = mw.util.wikiScript( 'rest' );
-		let url = `${restScript}/mws/v1/data-stash/${encodeURIComponent( key )}`;
+		let url = `${ restScript }/mws/v1/data-stash/${ encodeURIComponent( key ) }`;
 		if ( getGlobal ) {
 			url += '?global=1';
 		}
@@ -27,13 +19,13 @@ mws.datastash = {
 			return null;
 		}
 		if ( !response.ok ) {
-			throw new Error( `Failed to get stash data (${response.status})` );
+			throw new Error( `Failed to get stash data (${ response.status })` );
 		}
 		return response.json();
 	},
 	setInternal: async ( key, value, isGlobal ) => {
 		const restScript = mw.util.wikiScript( 'rest' );
-		const url = `${restScript}/mws/v1/data-stash/${encodeURIComponent( key )}`;
+		const url = `${ restScript }/mws/v1/data-stash/${ encodeURIComponent( key ) }`;
 		const body = {
 			data: value,
 			global: !!isGlobal
@@ -49,7 +41,7 @@ mws.datastash = {
 			body: JSON.stringify( body )
 		} );
 		if ( !response.ok ) {
-			throw new Error( `Failed to set stash data (${response.status})` );
+			throw new Error( `Failed to set stash data (${ response.status })` );
 		}
 	}
 };
